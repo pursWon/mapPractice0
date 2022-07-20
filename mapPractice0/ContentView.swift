caa1487f4ccd95e042157ee6be717e31dcd89eae
Wongi <hpurs95@gmail.com>
2022-07-21 00:13:52 +0900

    Initial Commit

---
+++ mapPractice0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mapPractice0
+//
+//  Created by MacBook Air on 2022/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mapPractice0/mapPractice0App.swift
@@ -0,0 +1,17 @@
+//
+//  mapPractice0App.swift
+//  mapPractice0
+//
+//  Created by MacBook Air on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct mapPractice0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
